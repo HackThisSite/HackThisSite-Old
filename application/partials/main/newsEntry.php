e9911f67a561f6d50b16cf66eb1e5847d81a6301
Joseph Moniz <joseph.moniz@gmail.com>
2011-10-28 16:24:10 -0700

    created partials

---
+++ application/partials/main/newsEntry.php
@@ -0,0 +1,21 @@
+<tr>
+  <td class="normal-td" style="font-size: 16px;">
+    <b>
+      <span style="font-size: 12px"><?php echo $date; ?>:&nbsp;&nbsp;</span>
+      <?php echo $title; ?>
+    </b>
+    <span style="display:none; font-size: 9px;"><br /></span>
+  </td>
+</tr>
+<tr>
+  <td class="normal-td" style="font-size: 10px;">
+    <br /><div class="news"><div align="left"><?php echo $body; ?></div></div>
+    <br />
+    <br />
+    <span style="font-size: 10px;">
+      <a href="/news/view/{id}">read more...</a>
+      <br />
+      <br />
+    </span>
+  </td>
+</tr>

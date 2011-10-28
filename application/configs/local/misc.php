38d91953830323cf61943ca41cf463e8160b0988
Joseph Moniz <joseph.moniz@gmail.com>
2011-10-27 21:58:49 -0700

    added a cluase

--- application/configs/local/misc.php
+++ application/configs/local/misc.php
@@ -1,4 +1,5 @@
 <?php
 // This is a local only config folder.
 // DO NOT CHECK ANY FILES IN THIS FOLDER IN!
+// SO ADD THIS ENIRE FOLDER TO YOUR .gitignore
 // All paramters defined here have the highest priority

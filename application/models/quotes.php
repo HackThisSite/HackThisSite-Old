00e27a5c30d0fee7b6c623915a94b60cce2f52ea
Joseph Moniz <joseph.moniz@gmail.com>
2011-10-30 09:56:37 -0700

    R#54 created quote backend model to work with apc/mongo

---
+++ application/models/quotes.php
@@ -0,0 +1,67 @@
+<?php
+
+class quotes extends mongoBase
+{
+    const KEY_ID    = "_id";
+    const KEY_QUOTE = "quote";
+
+    const CACHE_KEY_COUNT = "cache:quotes:count";
+    const CACHE_PREFIX    = "cache:quotes:i:";
+
+    private $mongo;
+    private $quotes;
+
+    public function __construct($mongoConnection)
+    {
+        $this->mongo  = $mongoConnection;
+        $this->quotes = $mongoConnection->quotes;
+    }
+
+    public function add($quote)
+    {
+        $this->quotes->insert(array(self::KEY_QUOTE => $quote));
+        $this->_invalidateCache();
+        $this->_populateCache();
+    }
+
+    public function remove($id)
+    {
+        $id = $this->_toMongoId($id);
+        $this->quotes->remove(array(self::KEY_ID => $id));
+        $this->_invalidateCache();
+        $this->_populateCache();
+    }
+
+    public function getRandom()
+    {
+        $count = apc_fetch(self::CACHE_KEY_COUNT);
+        if (!$count) { return false; }
+
+        return apc_fetch(self::CACHE_PREFIX . $count);
+    }
+
+    public function getAllFromDb()
+    {
+        return $this->quotes->find();
+    }
+
+    private function _invalidateCache()
+    {
+        $count = apc_fetch(self::CACHE_KEY_COUNT);
+        if ($count === false) { return false; }
+
+        $i = 0;
+        while ($i < $count) { apc_delete(self::CACHE_PREFIX . $i++); }
+        apc_delete(self::CACHE_KEY_COUNT);
+    }
+
+    private function _populateCache()
+    {
+        $i = 0;
+        foreach ($this->getAllFromDb() as $quote)
+        {
+            apc_store(self::CACHE_PREFIX . $i++);
+        }
+        apc_store(self::CACHE_KEY_COUNT, --$i);
+    }
+}

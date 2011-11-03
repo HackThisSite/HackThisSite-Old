2039603914389827de8747bd6b40e02057d82c7c
Brendan Mc <admin@bren2010.com>
2011-11-02 20:58:39 -0400

    Changed date lib to static.

--- library/date.php
+++ library/date.php
@@ -2,12 +2,12 @@
 class Date {
 	
 	// Day-precision dates
-	public function dayFormat($date) {
+	static public function dayFormat($date) {
 		return date('j M, Y', (int) $date);
 	}
 	
 	// Minute-precision dates
-	public function minuteFormat($date) {
+	static public function minuteFormat($date) {
 		return date('j M, Y H:i T', (int) $date);
 	}
 	

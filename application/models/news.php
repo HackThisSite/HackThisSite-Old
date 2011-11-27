6cfb05625a4a6d8c387bdb52b03205dd6f0ee81d
Joseph Moniz <joseph.moniz@gmail.com>
2011-11-27 10:51:08 -0800

    changes to news

--- application/models/news.php
+++ application/models/news.php
@@ -3,41 +3,42 @@ class news {
     const KEY_SERVER = "mongo:server";
     const KEY_DB     = "mongo:db";
 
+    const CACHE_PREFIX = "_cache:mongo:news:";
+
     var $db;
 
-    public function __construct(Mongo $mongo) {
+    public function __construct(Mongo $mongo)
+    {
         $db       = Config::get(self::KEY_DB);
         $this->db = $mongo->$db->content;
     }
 
-    public function getNewPosts($cache = true) {
+    public function getNewsPosts()
+    {
         $news = $this->realGetNewPosts();
         return $news;
     }
 
-    public function getNews($id, $cache = true) {
-        if ($cache && apc_exists('news_' . $id)) return apc_fetch('news_' . $id);
-
+    public function getNews($id, $cache = true)
+    {
         $news = $this->realGetNews($id);
-        if ($cache && !empty($news)) apc_add('news_' . $id, $news, 10);
         return $news;
     }
 
-    public function saveNews($title, $text, $commentable) {
-        $forums = new Forums;
-        $data = $forums->loginData();
-        $id = $data['id'];
-
+    public function saveNews($title, $text, $commentable)
+    {
         $entry = array('type' => 'news', 'title' => htmlentities($title), 'body' => $text, 'userId' => $id, 'date' => time(), 'commentable' => (bool) $commentable, 'ghosted' => false, 'flaggable' => false);
         $this->db->insert($entry);
     }
 
-    public function editNews($id, $title, $text, $commentable) {
+    public function editNews($id, $title, $text, $commentable)
+    {
         $this->db->update(array('_id' => $id), array('$set' => array(
             'title' => htmlentities($title), 'body' => $text, 'commentable' => (bool) $commentable)));
     }
 
-    public function deleteNews($id) {
+    public function deleteNews($id)
+    {
         $idLib = new Id;
         $query = array('type' => 'news', 'ghosted' => false);
         $keys = $idLib->dissectKeys($id, 'news');
@@ -59,7 +60,8 @@ class news {
         return true;
     }
 
-    public function realGetNewPosts() {
+    public function realGetNewPosts()
+    {
         return $this->db->find(
             array(
                 'type' => 'news',
@@ -69,7 +71,8 @@ class news {
          ->limit(10);
     }
 
-    public function realGetNews($id) {
+    public function realGetNews($id)
+    {
         $idLib = new Id;
 
         $query = array('type' => 'news', 'ghosted' => false);

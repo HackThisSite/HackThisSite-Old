4ca2135a75534413f811a9cff2b253c06adc5390
Joseph Moniz <joseph.moniz@gmail.com>
2011-11-27 10:53:54 -0800

    registration view

---
+++ application/views/main/register/index.php
@@ -0,0 +1,19 @@
+<form method="post" id="registration_form" target="/register/submit">
+  <p>
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="registration_username" />
+  </p>
+  <p>
+    <label for="email">Email:</label>
+    <input type="text" name="email" id="registration_email" />
+  </p>
+  <p>
+    <label for="password1">Password:</label>
+    <input type="password" name="password1" id="registration_password1" />
+  </p>
+  <p>
+    <label for="password2">Password:</label>
+    <input type="password" name="password2" id="registration_password2" />
+  </p>
+  <input type="submit" id="registration_submit" value="Register" />
+</form>
